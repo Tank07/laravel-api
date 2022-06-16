b30d3d89ff2cbdde0d4c4df992eead55c066194a
Tancredi Carluccio <tanc_@hotmail.it>
2022-06-17 00:44:09 +0200

    Creazione routes e controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function(){
+    $name = ['uno','due','tre'];
+
+    return response()->json(compact('name'));
+});
+
+Route::get('/posts', 'Api\PostController@index');
